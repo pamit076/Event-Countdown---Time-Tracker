207a8df4e1394713295fb8413dbf99f24d958270
Pamit Kumar Singh <pamit.singh@rapido.bike>
2024-08-28 14:41:03 +0530

    Initial Commit

---
+++ Event CountDown Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Event CountDown Tracker
+//
+//  Created by Pamit Kumar Singh on 28/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Event CountDown Tracker/Event_CountDown_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountDown_TrackerApp.swift
+//  Event CountDown Tracker
+//
+//  Created by Pamit Kumar Singh on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountDown_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
